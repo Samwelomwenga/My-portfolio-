00d08e9692fc082adfee571ca5b4880ce67f4299
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2022-06-10 22:15:49 +0300

    configured colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "neutral-white" :"hsl(160, 5%, 78%)",
+        "primary-light-green" :"hsl(145, 62%, 42%)",
+        "bg-dark-blue" :"hsl(210, 82%, 4%)",
+        "primary-light-blue" :"hsl(189, 58%, 53%)",
+        "neutral-light-blue" :"hsl(217, 92%, 42%)",
+        "primary-dark-green" :"hsl(185, 96%, 19%)",
+        "neutral-dark-green" :"hsl(180, 64%, 2%)",
+      },
+    },
   },
   plugins: [],
 }
