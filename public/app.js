0e24b8df9f056f0664231b35088c5df7d1a2c403
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2022-08-04 15:04:21 +0300

    Remve unnecessary file

--- public/app.js
+++ public/app.js
@@ -1,34 +1,32 @@
 // start  of hamberger menu code
 
-const primarynav=document.querySelector("#primary-navigation");
-const navtoggle=document.querySelector(".mobile-nav-toggle");
+const primarynav = document.querySelector("#primary-navigation");
+const navtoggle = document.querySelector(".mobile-nav-toggle");
 
-navtoggle.addEventListener("click", ()=>{
-    const visibility= primarynav.getAttribute("data-visible")
-    if(visibility==="false"){
-        primarynav.setAttribute("data-visible",true);
-        navtoggle.setAttribute("aria-expanded",true);
-    }else if(visibility==="true") {
-        primarynav.setAttribute("data-visible",false);
-        navtoggle.setAttribute("aria-expanded",false);
-    } 
-    });
-    document.querySelectorAll(".nav-link").forEach( n=>n.addEventListener("click",()=>{
-        primarynav.setAttribute("data-visible",false);
-        navtoggle.setAttribute("aria-expanded",false);
-
-
-    }))
+navtoggle.addEventListener("click", () => {
+  const visibility = primarynav.getAttribute("data-visible");
+  if (visibility === "false") {
+    primarynav.setAttribute("data-visible", true);
+    navtoggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primarynav.setAttribute("data-visible", false);
+    navtoggle.setAttribute("aria-expanded", false);
+  }
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    primarynav.setAttribute("data-visible", false);
+    navtoggle.setAttribute("aria-expanded", false);
+  })
+);
 
 // End of hamberger menu code
 
-
-
-const scrollUp=document.querySelector("#scrollToTop");
-scrollUp.addEventListener("click", ()=>{
-    window.scrollTo({
-        top : 0,
-        left: 0,
-        behavior: "smooth",
-    });
+const scrollUp = document.querySelector("#scrollToTop");
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 });
